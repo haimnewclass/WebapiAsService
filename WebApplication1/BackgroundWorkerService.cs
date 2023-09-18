d4c75202e44db8ec048b5282ad31988eb7c7831e
haimnewclass <haim@newclass.co.il>
2023-09-19 01:23:40 +0300

    Add project files.

---
+++ WebApplication1/BackgroundWorkerService.cs
@@ -0,0 +1,39 @@
+﻿
+// generate classs BackgroundWorkerService that implement IHostedService 
+using System.Diagnostics;
+using System.Threading;
+
+public class BackgroundWorkerService : BackgroundService
+{
+   
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // 
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            // write to console
+            Console.WriteLine("Hello World");
+            //sleep for 5 seconds
+            await Task.Delay(5000,stoppingToken);
+        }
+    }
+}
+
+
+
+
+public static class WindowsServiceHelpers
+{
+    public static bool IsWindowsService()
+    {
+        bool isService = false;
+
+        using (var process = Process.GetCurrentProcess())
+        {
+            isService = process.SessionId == 0 && process.ProcessName == "YOUR_SERVICE_NAME";
+        }
+
+        return isService;
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.ComponentModel;
+
+// Create Host for Worker Service
+var host = Host.CreateDefaultBuilder(args).UseWindowsService()    
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddHostedService<BackgroundWorkerService>();
+    })
+    .Build();
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Create WebHost for ASP.NET Core Application
+            var webHost = new WebHostBuilder()
+                .UseKestrel()
+                .ConfigureServices(services =>
+                {
+                    services.AddControllers();  // Add MVC services
+                }).UseUrls("http://localhost:5009", "https://localhost:5001")   
+                .Configure(app =>
+                {
+                    app.UseRouting();
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapControllers();  // Map controller routes
+                    });
+                    
+                    app.Run(async context =>
+                    {
+                        await context.Response.WriteAsync("Fallback handler");
+                    });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .Build();
+
+// Run both concurrently
+await Task.WhenAll(host.RunAsync(), webHost.RunAsync());
+         
+/*
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry();
+//builder.Host.UseWindowsService();
+
+// Configure the HTTP request pipeline.
+
+if (WindowsServiceHelpers.IsWindowsService())
+{
+    //  app.UseWindowsService();
+   // builder.Host.UseWindowsService();
+
+}
+
+
+builder.WebHost.UseUrls("http://localhost:5000", "https://localhost:5001");
+var app = builder.Build();
+app.MapControllers();
+
+
+// Task.Run(() => { app.Run(); });
+
+await Task.WhenAll(host.RunAsync(), app.RunAsync());
+*/
